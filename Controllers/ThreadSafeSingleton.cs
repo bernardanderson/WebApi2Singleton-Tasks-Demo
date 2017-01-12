ab9e75cd05d5773e52b5be8317683ad2048b7939
bernardanderson <bernard.anderson@gmail.com>
2017-01-12 09:07:13 -0600

    Added basic Thread-safe singleton with a stored list

---
+++ Controllers/ThreadSafeSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiSingleton.Controllers
+{
+    public class ThreadSafeSingleton
+    {
+        private List<int> _ints;
+
+        private ThreadSafeSingleton()
+        {
+        }
+
+        private static ThreadSafeSingleton Instance
+        {
+            get { return Nested.instance; }
+        }
+
+        private class Nested
+        {
+            static Nested()
+            {
+            }
+
+            internal static readonly ThreadSafeSingleton instance = new ThreadSafeSingleton();
+        }
+
+        public void AddInts (int sentInts)
+        {
+            _ints.Add(sentInts);
+        } 
+
+        public List<int> ReturnInts()
+        {
+            return _ints;
+        }
+
+
+    }
+}
